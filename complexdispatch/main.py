ac6a73b1766f00e5705658aca7203045538d07a8
Skyler Curtis <skyler@cyborgsecurity.com>
2025-04-02 05:47:08 -0400

    renamed main file

--- complexdispatch/main.py
+++ complexdispatch/main.py
@@ -9,10 +9,7 @@
 #   Copyright (C) 2006 Python Software Foundation.
 # See C source code for _functools credits/copyright
 
-__all__ = ["update_wrapper", "wraps", "WRAPPER_ASSIGNMENTS", "WRAPPER_UPDATES",
-           "total_ordering", "cache", "cmp_to_key", "lru_cache", "reduce",
-           "partial", "partialmethod", "singledispatch", "singledispatchmethod",
-           "cached_property", "Placeholder"]
+__all__ = ["singledispatch", "singledispatchmethod"]
 
 from abc import get_cache_token
 from collections import namedtuple
@@ -21,6 +18,8 @@ from operator import itemgetter
 from reprlib import recursive_repr
 from types import GenericAlias, MethodType, MappingProxyType, UnionType
 from _thread import RLock
+
+from functools import update_wrapper, wraps
 
 ################################################################################
 ### update_wrapper() and wraps() decorator
@@ -32,49 +31,6 @@ from _thread import RLock
 WRAPPER_ASSIGNMENTS = ("__module__", "__name__", "__qualname__", "__doc__",
                        "__annotate__", "__type_params__")
 WRAPPER_UPDATES = ("__dict__",)
-def update_wrapper(wrapper,
-                   wrapped,
-                   assigned = WRAPPER_ASSIGNMENTS,
-                   updated = WRAPPER_UPDATES):
-    """Update a wrapper function to look like the wrapped function
-
-       wrapper is the function to be updated
-       wrapped is the original function
-       assigned is a tuple naming the attributes assigned directly
-       from the wrapped function to the wrapper function (defaults to
-       functools.WRAPPER_ASSIGNMENTS)
-       updated is a tuple naming the attributes of the wrapper that
-       are updated with the corresponding attribute from the wrapped
-       function (defaults to functools.WRAPPER_UPDATES)
-    """
-    for attr in assigned:
-        try:
-            value = getattr(wrapped, attr)
-        except AttributeError:
-            pass
-        else:
-            setattr(wrapper, attr, value)
-    for attr in updated:
-        getattr(wrapper, attr).update(getattr(wrapped, attr, {}))
-    # Issue #17482: set __wrapped__ last so we don't inadvertently copy it
-    # from the wrapped function when updating __dict__
-    wrapper.__wrapped__ = wrapped
-    # Return the wrapper so this can be used as a decorator via partial()
-    return wrapper
-
-def wraps(wrapped,
-          assigned = WRAPPER_ASSIGNMENTS,
-          updated = WRAPPER_UPDATES):
-    """Decorator factory to apply update_wrapper() to a wrapper function
-
-       Returns a decorator that invokes update_wrapper() with the decorated
-       function as the wrapper argument and the arguments to wraps() as the
-       remaining arguments. Default arguments are as for update_wrapper().
-       This is a convenience function to simplify applying partial() to
-       update_wrapper().
-    """
-    return partial(update_wrapper, wrapped=wrapped,
-                   assigned=assigned, updated=updated)
 
 
 ################################################################################
@@ -230,7 +186,7 @@ def _find_impl_match(cls_obj, registry):
                 match_args = get_args(get_args(match)[0])
                 t_args = get_args(get_args(t)[0])
                 if len(match_args) == len(t_args):
-                    raise RuntimeError("Ambiguous dispatch: {} or {}".format( match, t))
+                    raise RuntimeError(f"Ambiguous dispatch: {match} or {t}")
 
                 elif len(t_args)<len(match_args):
                     match = t
@@ -245,8 +201,7 @@ def _find_impl_match(cls_obj, registry):
             if (t in registry and t not in cls.__mro__
                               and match not in cls.__mro__
                               and not issubclass(match, t)):
-                raise RuntimeError("Ambiguous dispatch: {} or {}".format(
-                    match, t))
+                raise RuntimeError(f"Ambiguous dispatch: {match} or {t}")
             break
         if t in registry:
             match = t
