8a7633397f2e912963a2675afa2a2f12c37b643a
Skyler Curtis <skylerr.curtis@gmail.com>
2025-04-03 05:03:24 -0400

    restructuring

--- complexdispatch/main.py
+++ complexdispatch/main.py
@@ -2,7 +2,7 @@
 # Credit and huge thanks to:
 # Nick Coghlan <ncoghlan at gmail.com>,
 # Raymond Hettinger <python at rcn.com>,
-# Łukasz Langa <lukasz at langa.pl>.
+# Lukasz Langa <lukasz at langa.pl>.
 # Copyright (C) 2006 Python Software Foundation.
 
 __all__ = ["singledispatch", "singledispatchmethod"]
@@ -30,88 +30,24 @@ WRAPPER_ASSIGNMENTS = ("__module__", "__name__", "__qualname__", "__doc__",
                        "__annotate__", "__type_params__")
 WRAPPER_UPDATES = ("__dict__",)
 
+from functools import update_wrapper
+from complexdispatch.mro import *
+from complexdispatch.find_impl import _find_impl
+from complexdispatch.utils import _pep585_registry_matches
 
 ################################################################################
 ### singledispatch() - single-dispatch generic function decorator
 ################################################################################
 
-
-def _pep585_registry_matches(cls, registry):
-    from typing import get_origin
-    return (i for i in registry.keys() if get_origin(i) == cls)
-
-def _find_impl_match(cls_obj, registry):
-    """Returns the best matching implementation from *registry* for type *cls_obj*.
-
-    Where there is no registered implementation for a specific type, its method
-    resolution order is used to find a more generic implementation.
-
-    Note: if *registry* does not contain an implementation for the base
-    *object* type, this function may return None.
-
-    """
-    cls = cls_obj if isinstance(cls_obj, type) else cls_obj.__class__
-    mro = _compose_mro(cls, registry.keys())
-    match = None
-
-    from typing import get_origin, get_args
-
-    if (not isinstance(cls_obj, type) and
-        len(cls_obj) > 0 and # dont try to match the types of empty containers
-        any(_pep585_registry_matches(cls, registry))):
-        # check containers that match cls first
-        for t in _pep585_registry_matches(cls, registry):
-            if not all((isinstance(i, get_args(t)) for i in cls_obj)):
-                continue
-
-            if match is None:
-                match = t
-
-            else:
-                match_args = get_args(get_args(match)[0])
-                t_args = get_args(get_args(t)[0])
-                if len(match_args) == len(t_args):
-                    raise RuntimeError(f"Ambiguous dispatch: {match} or {t}")
-
-                elif len(t_args)<len(match_args):
-                    match = t
-
-    if match:
-        return match
-
-    for t in mro:
-        if match is not None:
-            # If *match* is an implicit ABC but there is another unrelated,
-            # equally matching implicit ABC, refuse the temptation to guess.
-            if (t in registry and t not in cls.__mro__
-                              and match not in cls.__mro__
-                              and not issubclass(match, t)):
-                raise RuntimeError(f"Ambiguous dispatch: {match} or {t}")
-            break
-        if t in registry:
-            match = t
-
-    return match
-
-def _find_impl(cls_obj, registry):
-    return registry.get(
-        _find_impl_match(cls_obj, registry)
-    )
-
-
 def singledispatch(func):
-    """Single-dispatch generic function decorator.
+    """functools.singledispatch with support for PEP-585
 
     Transforms a function into a generic function, which can have different
     behaviours depending upon the type of its first argument. The decorated
     function acts as the default implementation, and additional
     implementations can be registered using the register() attribute of the
-    generic function.
+    generic function. Adds support for collections typing such as `list[str]`.
     """
-    # There are many programs that use functools without singledispatch, so we
-    # trade-off making singledispatch marginally slower for the benefit of
-    # making start-up of such applications slightly faster.
-    import weakref
 
     registry = {}
     dispatch_cache = weakref.WeakKeyDictionary()
